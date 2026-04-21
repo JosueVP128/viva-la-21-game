5fd59e19a4154abd158788c9827b46b2da92200b
Josue Vaca Pizano <vacajos125@gmail.com>
2026-04-21 17:19:42 -0500

    Fixed make_deck() code

--- src/project.py
+++ src/project.py
@@ -4,9 +4,9 @@ import pygame
 # Makes deck of cards
 def make_deck():
     deck = []
-    suits = ["Hearts", "Diamonds", "Spades, Clubs"]
+    suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
     values = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8,
-              "7": 7, "8": 8, "9": 9, "10": 10, "A": 11}
+              "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": 11}
     
     for suit in suits:
         for value in values:
