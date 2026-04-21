72c42e2d7244227df62904b7e5b740697ac5e42d
Josue Vaca Pizano <vacajos125@gmail.com>
2026-04-21 16:22:52 -0500

    Made the deck of cards

--- src/project.py
+++ src/project.py
@@ -1,1 +1,15 @@
 import random
+import pygame
+
+def make_deck():
+    deck = []
+    suits = ["Hearts", "Diamonds", "Spades, Clubs"]
+    values = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8,
+              "7": 7, "8": 8, "9": 9, "10": 10, "A": 11}
+    
+    for suit in suits:
+        for value in values:
+            deck.append((value, values[value], suit))
+    
+    random.shuffle(deck)
+    return deck
