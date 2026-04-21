cf873e80a6c018ddb78295a099e826941aa7700c
Josue Vaca Pizano <vacajos125@gmail.com>
2026-04-21 16:41:35 -0500

    Created card dealing and hand value calculation logic

--- src/project.py
+++ src/project.py
@@ -1,6 +1,7 @@
 import random
 import pygame
 
+# Makes deck of cards
 def make_deck():
     deck = []
     suits = ["Hearts", "Diamonds", "Spades, Clubs"]
@@ -13,3 +14,19 @@ def make_deck():
     
     random.shuffle(deck)
     return deck
+
+# Deals the cards
+def deal_card(deck, hand):
+    card = deck.pop()
+    hand.append(card)
+
+# Hand value calculating
+def calculate_hand(hand):
+    total = sum(card[1] for card in hand)
+    ace = sum(1 for card in hand if card[0] == "A")
+
+    while total > 21 and ace:
+        total -= 10
+        ace -= 1
+    
+    return total
